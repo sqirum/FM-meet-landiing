ce7c09f5df0dbf41b3c146220c4467899d2e70ca
sqirum <pauloslh94@gmail.com>
2022-09-06 23:54:50 +0100

    update tailwind config with colors test new classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
         body: ['Red Hat Display']
       }
     },
+    colors: {
+      primary: {
+        100: '#8FE3F9',
+        200: '#4D96A9'
+      },
+      secondary: {
+        100: '#D9B8FF',
+        200: '#855FB1'
+      },
+      neutral: {
+        100: '#FAFAFA',
+        200: '#87879D',
+        300: '#28283D'
+      }
+    },
   },
   plugins: [],
 }
