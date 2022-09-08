1c994ef5aa04d47ac2a714b9a0fb5965ad0d7daa
sqirum <pauloslh94@gmail.com>
2022-09-08 19:33:13 +0100

    update exercise

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,25 +5,25 @@ module.exports = {
     extend: {
       fontFamily: {
         body: ['Red Hat Display']
-      }
-    },
-    colors: {
-      primary: {
-        100: '#8FE3F9',
-        200: '#71C0D4',
-        300: '#4D96A9'
       },
-      secondary: {
-        100: '#D9B8FF',
-        200: '#B18BDD',
-        300: '#855FB1'
+      colors: {
+        primary: {
+          100: '#8FE3F9',
+          200: '#71C0D4',
+          300: '#4D96A9'
+        },
+        secondary: {
+          100: '#D9B8FF',
+          200: '#B18BDD',
+          300: '#855FB1'
+        },
+        neutral: {
+          100: '#FAFAFA',
+          200: '#87879D',
+          300: '#28283D'
+        },
+        white: '#FFFFFF'
       },
-      neutral: {
-        100: '#FAFAFA',
-        200: '#87879D',
-        300: '#28283D'
-      },
-      white: '#FFFFFF'
     },
   },
   plugins: [],
